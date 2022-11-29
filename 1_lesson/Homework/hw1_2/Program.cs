e9a0141a0f0a58f43868142809164c490511557e
ZinnatullinRustem <sunnypenguin@mail.ru>
2022-11-30 03:12:23 +0500

    Add Homework hw1_2

---
+++ 1_lesson/Homework/hw1_2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Вводим число. Проверяем чётное или нет
+
+Console.Write("Enter the number ");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0)
+Console.Write("Even number");
+else 
+Console.Write("Odd number ");
