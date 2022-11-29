229da6f86618d59a84e772781e67ff208847df56
ZinnatullinRustem <sunnypenguin@mail.ru>
2022-11-30 02:17:19 +0500

    Add Task 1_1

---
+++ 1_lesson/1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет,
+// является ли первое число квардратом второго.
+
+Console.WriteLine("Введите число 1");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 == num2*num2)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
