78c3e708e16585021695b4756e86ca4190288b7b
ZinnatullinRustem <sunnypenguin@mail.ru>
2022-11-30 03:14:49 +0500

    Add Homework hw1_3

---
+++ 1_lesson/Homework/hw1_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Вводим число N. Выводим все четные числа от 1 до N
+
+Console.Write("Enter the number ");
+int num = int.Parse(Console.ReadLine());
+if (num < 2)
+{
+Console.Write("There are no even numbers less than ");
+Console.Write(num);
+}
+else {
+int i = 2;
+Console.Write("Even numbers less than ");
+Console.Write(num);
+Console.WriteLine("");
+while (num >= i) 
+{
+    Console.WriteLine(i);
+    i = i + 2;
+}
+}
