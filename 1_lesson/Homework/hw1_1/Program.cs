0372681b3a1a92a497ece09c86edf55e28f5080a
ZinnatullinRustem <sunnypenguin@mail.ru>
2022-11-30 03:10:08 +0500

    Add Homework hw1_1

---
+++ 1_lesson/Homework/hw1_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вводим 3 числа. Выводим максимальное число
+
+Console.Write("Enter the first number ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Enter the second number ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Enter the third number ");
+int num3 = int.Parse(Console.ReadLine());
+int max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+Console.Write("Max number ");
+Console.WriteLine(max);
