646648279c2c0acf07f03bc97cd3b31364ea5f8e
ZinnatullinRustem <sunnypenguin@mail.ru>
2022-11-30 03:06:41 +0500

    Add Homework hw1_0

---
+++ 1_lesson/Homework/hw1_0/Program.cs
@@ -0,0 +1,29 @@
+﻿// Программа принимает 2 числа и выдает макс и мин. Если равны, пишет об этом
+Console.Write("Enter the first number ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Enter the second number ");
+int num2 = int.Parse(Console.ReadLine());
+int max = num1;
+int min = num2;
+if (num1 > num2) 
+{
+    max = num1; 
+    min = num2;
+    Console.Write("Max number ");
+    Console.WriteLine(max);
+    Console.Write("Min number ");
+    Console.WriteLine(min);
+}
+else if (num1 < num2)
+{
+    max = num2;
+    min = num1;
+    Console.Write("Max number ");
+    Console.WriteLine(max);
+    Console.Write("Min number ");
+    Console.WriteLine(min);
+}
+else 
+{
+    Console.Write("Same numbers");
+}
