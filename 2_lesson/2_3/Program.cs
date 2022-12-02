9368836a748dff0f89fa497eaf83f1a4a1520113
ZinnatullinRustem <sunnypenguin@mail.ru>
2022-12-03 01:45:49 +0500

    Task 2_3

---
+++ 2_lesson/2_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// 3. Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23.
+
+string Krat (int num)
+{
+    if ( num % 7 == 0 && num % 23 ==0)
+    {
+        return "Кратно";
+    }
+
+    else 
+    {
+        return "Не кратно";
+    }
+}
+
+Console.Write("Enter the first number ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(Krat(num));
