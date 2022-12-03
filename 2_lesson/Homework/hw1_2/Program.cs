8c5f3e148fe562f3643b2317af1aeb5fa8ccbcfc
ZinnatullinRustem <sunnypenguin@mail.ru>
2022-12-04 01:12:57 +0500

    Homework 1_2

--- 2_lesson/Homework/hw1_2/Program.cs
+++ 2_lesson/Homework/hw1_2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3. Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет.
+
+string ThreeNumber(int num)
+{
+if (num > 99)
+{
+while (num > 999)
+{
+    num = num / 10 ;
+}
+return $"Третья цифра числа это {num % 10} ";
+}
+else 
+    return "Нет третьей цифры заданного числа";
+}
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(ThreeNumber(num));
