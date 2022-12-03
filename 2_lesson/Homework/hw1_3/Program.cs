33c515c899cafffdf6839d376d3c39dc55c30975
ZinnatullinRustem <sunnypenguin@mail.ru>
2022-12-04 01:27:36 +0500

    Homework 1_3

---
+++ 2_lesson/Homework/hw1_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// 4. Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+string DaysNumber(int num)
+{
+if (num > 0 && num < 8)
+{
+    if (num == 6 || num == 7)
+    {
+        return "Это выходной";
+    }
+    else
+    return "Это будний день";
+}
+else 
+    return "Ты дурачок? В неделе 7 дней!";
+}
+Console.WriteLine("Введите день недели цифрой");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(DaysNumber(num));
