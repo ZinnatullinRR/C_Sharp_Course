11a11d84ec342a91e1276b910a063dc4eede023a
ZinnatullinRustem <sunnypenguin@mail.ru>
2022-12-04 02:01:10 +0500

    Homework 1_1

---
+++ 2_lesson/Homework/hw1_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// 2. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого числа.
+
+int FirstNumber (int num)
+{
+    return num / 100;
+}
+int LastNumber (int num)
+{
+    return num % 10;
+}
+int num = new Random().Next(100,999);
+//Console.WriteLine(MaxNumber(new Random().Next(100,999)));
+Console.WriteLine($"Random number {num}");
+Console.WriteLine($"First number {FirstNumber(num)}");
+Console.WriteLine($"Last number {LastNumber(num)}");
+Console.WriteLine($"Result {FirstNumber(num) * 10 + LastNumber(num)}");
+//Console.WriteLine(FirstNumber(num)+LastNumber(num));
