230ea6d5d097b58a3c472aad89fefa277005d478
ZinnatullinRustem <sunnypenguin@mail.ru>
2022-12-05 02:44:05 +0500

    Homework 1_0

---
+++ 3_lesson/Homework/hw1_0/Program.cs
@@ -0,0 +1,20 @@
+﻿// 1. Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+
+string FiveNumber(int num)
+{
+    if (num > 9999 && num < 100000)
+        if (num / 10000 == num % 10 && num / 1000 % 10 == num / 10 % 10)
+        {
+            return $"Это число палиндром";
+        }
+        else
+            return $"Это число не палиндром";
+    else
+        return "Это не пятизначное число";
+}
+
+
+Console.WriteLine("Введите пятизначное число");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(FiveNumber(num));
