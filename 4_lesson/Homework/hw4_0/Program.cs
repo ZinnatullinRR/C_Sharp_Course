3d72a05070b146dedd356f08b2c8c1f34146bf35
ZinnatullinRustem <sunnypenguin@mail.ru>
2022-12-27 03:25:39 +0500

    Homework hw4_0

---
+++ 4_lesson/Homework/hw4_0/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B. Без модуля Math, используем цикл for.
+
+int NumDegree(int num1, int num2)
+{
+    int num_res = 1;
+
+    for (int i = 1; i <= num2; i++)
+    {
+
+        num_res = num_res* num1;
+    }
+
+    return num_res;
+}
+
+Console.WriteLine("Введите число A");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B>=0");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine(NumDegree(num1, num2));
